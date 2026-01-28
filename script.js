15e1585e54bdd8f3ba513ccdbac22d7c7c57f475
Nitesh Nagar <159615163+xitesh@users.noreply.github.com>
2026-01-28 19:52:16 +0530

    Enhance game interface with input fields and buttons; update styles for layout and responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.querySelector('.theme').addEventListener('click', ()=> {
   document.body.classList.toggle('light')
 })
+
+
+
+const guess = document.querySelector('.input-guess').textContent
+
+const hitMe = document.querySelector('.hit-me').addEventListener('click', ()=> {
+  guess = guess.textContent
+  if(!guess) alert('f#ck you');
+
+  // if(guess)
+})
