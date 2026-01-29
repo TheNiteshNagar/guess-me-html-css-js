a55622e043093059218e63ac0a27e436368ea507
Nitesh Nagar <159615163+xitesh@users.noreply.github.com>
2026-01-29 12:33:01 +0530

    add game over functionality

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let movesCount = 0
 let randomNumber = getRandomNumber()
 let winningSound = new Audio('./public/audio/winning-sound.mp3')
 let winningEffect = document.querySelector('.winning-effect')
+let loseSound = new Audio('./public/audio/lose-sound.mp3')
+let loseEffect = document.querySelector('.lose-effect')
 
 const inputBox = document.querySelector('.input-guess')
 const hint = document.querySelector('.hint')
@@ -20,21 +22,56 @@ function game() {
 
   // if there is no guess return and show a hint
   if (!guess) {
-    hint.textContent = 'Hint: Are you idiot 😒'
+    hint.textContent = 'Are you idiot 😒'
     return
   }
 
   // convert guess into number
   guess = Number(guess)
 
+  // check is value is between 1 to 100
+  if (guess < 1 || guess > 100) {
+    hint.textContent = 'Enter value between 1 to 100 😤'
+    return
+  }
+
+  if (movesCount === 10) {
+    // if movesCount react to 10 player will be lose
+    hint.style = 'color: red'
+    hint.textContent = 'You Lose 😭'
+
+    // playing lose sound
+    loseEffect.classList.add('enable')
+    loseSound.currentTime = 0
+    loseSound.volume = 0.4
+    loseSound.play()
+
+    // after 5 sec reset everything
+    setTimeout(() => {
+      loseEffect.classList.remove('enable')
+      movesCount = 0
+      moves.textContent = ''
+      hint.textContent = 'New Game Starting...'
+      userGuess.textContent = ''
+    }, 5000);
+
+    // after 7 sec start the game again
+    setTimeout(() => {
+      hint.textContent = 'Guess Again!'
+      randomNumber = getRandomNumber()
+    }, 7000);
+
+  }
+
   // let see guess is equal to number or less or greater
   if (guess === randomNumber) {
     hint.style = 'color: blue'
-    hint.textContent = 'Tum Jeet Gaye Bhai 🎉'
+    hint.textContent = 'You Won 🎊'
     
     // play winning sound
-    winningSound.currentTime = 0
     winningEffect.classList.add('enable')
+    winningSound.currentTime = 0
+    winningSound.volume = 0.5
     winningSound.play()
     
     // after 5 sec reset everything
@@ -81,16 +118,22 @@ function game() {
 }
 
 
+
+
 // play game on event like button click or enter or space button
 
 // hit me button
+window.addEventListener('keypress' || 'click', () => {
+  inputBox.focus()
+} )
+
 document.querySelector('.hit-me').addEventListener('click', () => {
   game()
 })
 
-// on kepress enter or space
 window.addEventListener('keypress', (ev) => {
-  if (ev.code === 'Enter' || ev.code === 'Space') {
+  // ev.code for desktop and ev.key for mobile devices
+  if (ev.code === 'Enter' || ev.code === 'Space' || ev.key === 'Enter') {
     game()
   }
 })
